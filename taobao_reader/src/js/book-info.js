c666648594fb96287cc62a2ae692a3969846fc7a
李洪 <lh104439@ucweb.com>
2016-10-27 23:25:21 +0800

    加入md5

--- taobao_reader/src/js/book-info.js
+++ taobao_reader/src/js/book-info.js
@@ -2,15 +2,25 @@
  * Created by LeeHong on 16/10/27.
  */
 $(document).ready(function(){
-    var val = getQueryString("tsid");
-    // alert("tsid = " + val);
-    alert("url = " + location.href);
     var tsid = getQueryString("tsid");
-    alert("tsid = " + tsid);
+    var placeId = getQueryString("placeId");
+    var appVer = getQueryString("appVer");
+
+    var data = {"bookId": 172679, "timestamp": Date.parse(new Date())/1000 };
+    var md5key = '37e81a9d8f02596e1b895d07c171d5c9';
+    var sign = md5(data+md5key);
+    alert("sign = " + sign);
 
     $("#gotoChapter").click(function () {
-        $.post("http://tbwalden.demowx.uae.uc.cn/andapi/book/info",
-            {"data": {"bookId": 172679, "timestamp": Date.parse(new Date())/1000 }},
+        $.post("http://tbwalden.ishuqi.com/andapi/book/info",
+            {
+                "data": data,
+                "encryptType": -1,
+                "tsid": tsid,
+                "placeId": placeId,
+                "appVer": appVer,
+                "sign": sign
+            },
             function (data, status) {
                 alert("返回的数据是：" + data.status + ", 返回的状态是：" + status);
             })
