f81c0386732036b1120159f4c94328ad8613e3dc
李洪 <lh104439@ucweb.com>
2016-11-09 00:08:10 +0800

    test2

--- taobao_reader/src/js/book-info.js
+++ taobao_reader/src/js/book-info.js
@@ -137,10 +137,9 @@ Book.prototype = {
 
         // 作者的作品
         aLi.length = 0;
-        var showBookCover = this.showBookCover;
-        alert(typeof showBookCover);
         for(var i = 0, count = this.proList.bookList.length; i < count; ++i) {
-            aLi.push("<li><a href='#' onclick='showBookCover("+this.proList.bookList[i]+");return false;'>"
+            // aLi.push("<li><a href='#' onclick='showBookCover("+this.proList.bookList[i]+");return false;'>"
+            aLi.push("<li><a href='#' onclick='alert("+this.proList.bookList[i].bookName+")'>"
                 + this.proList.bookList[i].bookName +"</a></li>");
         }
         $(".author-other-books .title").after("<nav><ul>" + aLi.join("") + "</ul></nav>");
